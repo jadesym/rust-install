18c1a90c57dfeae77a52298bda141126c689d093
Kevin Fu <kevin.fu808@gmail.com>
2022-01-05 01:23:13 -0800

    Adding copy of code to copy to ~/.profile file for PATH env variable

--- wsl/rust-install.sh
+++ wsl/rust-install.sh
@@ -2,6 +2,13 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Shell may need to be restarted in order for following commands to work 
 
+# Also, make sure to add ~/.cargo/bin to the PATH environment variable.
+# The following code can be copied in:
+# # Configuring Rust development environment
+# if [ -d "$HOME/.cargo/bin" ] ; then
+#     PATH="$PATH:$HOME/.cargo/bin"
+# fi
+
 # Update rustup
 rustup update
 
